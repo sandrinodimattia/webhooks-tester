38ea530822ca31022ffc3454b39366037485850b
Sandrino Di Mattia <contact@sandrino.be>
2017-06-13 10:03:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Webhooks Tester
+
+Install:
+
+```bash
+npm install -g webhooks-tester
+```
+
+Run:
+
+```bash
+webhooks-tester
+```
+
+You can then call the internal or public URL and the full request will be shown in the console:
+
+![Screenshot](./media/screenshot.png)
---
+++ index.js
@@ -0,0 +1,41 @@
+const pino = require('pino');
+const ngrok = require('ngrok');
+const express = require('express');
+const bodyParser = require('body-parser');
+const absoluteUrl = require('absolute-url');
+
+const logger = require('./logger');
+
+const app = express();
+app.enable('trust proxy');
+app.use(absoluteUrl());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res) => {
+  const log = {
+    url: req.url,
+    query: req.query,
+    headers: req.headers,
+    body: req.body
+  };
+  logger.info(log, `${req.method} ${req.absoluteUrl()}`);
+  res.json({ message: 'ok' });
+});
+
+const port = process.env.PORT || 3000;
+ngrok.connect(port, (ngrokError, url) => {
+  if (ngrokError) {
+    logger.error(ngrokError, 'Error connecting to ngrok');
+    return;
+  }
+
+  app.listen(port, (err) => {
+    if (err) {
+      logger.error(err);
+      return;
+    }
+
+    logger.info(`Local URL: http://localhost:${port}`);
+    logger.info(`Public URL: ${url}`);
+  });
+});
